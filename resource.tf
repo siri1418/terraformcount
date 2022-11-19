4f88f1810c3b1c6e9f1feb1f4c62d33cb6636f56
srividya <srividya.ss1418@gmail.com>
2022-11-19 16:49:31 +0530

    manual using count

--- resource.tf
+++ resource.tf
@@ -10,26 +10,10 @@ resource "azurerm_virtual_network" "virtualnetwork" {
   resource_group_name = azurerm_resource_group.resource.name
 }
 resource "azurerm_subnet" "sub1" {
-  name                 = var.sub.name[0]
+  count = 4
+  name                 = var.sub.name[count.index]
   resource_group_name  = azurerm_resource_group.resource.name
   virtual_network_name = azurerm_virtual_network.virtualnetwork.name
-  address_prefixes     = [var.sub.address_prefixes[0]]
+  address_prefixes     = [var.sub.address_prefixes[count.index]]
 }
-resource "azurerm_subnet" "sub2" {
-  name                 = var.sub.name[1]
-  resource_group_name  = azurerm_resource_group.resource.name
-  virtual_network_name = azurerm_virtual_network.virtualnetwork.name
-  address_prefixes     = [var.sub.address_prefixes[1]]
-}
-resource "azurerm_subnet" "sub3" {
-  name                 = var.sub.name[2]
-  resource_group_name  = azurerm_resource_group.resource.name
-  virtual_network_name = azurerm_virtual_network.virtualnetwork.name
-  address_prefixes     = [var.sub.address_prefixes[2]]
-}
-resource "azurerm_subnet" "sub4" {
-  name                 = var.sub.name[3]
-  resource_group_name  = azurerm_resource_group.resource.name
-  virtual_network_name = azurerm_virtual_network.virtualnetwork.name
-  address_prefixes     = [var.sub.address_prefixes[3]]
-}
+ 
