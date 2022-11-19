da2cbe44b9701cac9710ab1bde280cea1473c46c
srividya <srividya.ss1418@gmail.com>
2022-11-19 17:03:15 +0530

    manual using length

--- resource.tf
+++ resource.tf
@@ -10,7 +10,7 @@ resource "azurerm_virtual_network" "virtualnetwork" {
   resource_group_name = azurerm_resource_group.resource.name
 }
 resource "azurerm_subnet" "sub1" {
-  count = 4
+  count = length(var.sub.name)
   name                 = var.sub.name[count.index]
   resource_group_name  = azurerm_resource_group.resource.name
   virtual_network_name = azurerm_virtual_network.virtualnetwork.name
